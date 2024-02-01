612db9d634a7c82908cef8c4c2b74e1277ad5948
Benoit Quette <benoit.quette@gmail.com>
2024-02-01 10:20:54 +0100

    removed API only middleware and addded mix auth in same middleware file

--- middlewares/apiKey.js
+++
@@ -1,4 +0,0 @@
-module.exports.isApiKeyProvided = (req, res, next) => {
-  const apiKey = req.get("API-Key");
-  !apiKey || apiKey !== process.env.API_KEY ? next() : res.sendStatus(401);
-};
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,17 @@
-module.exports.isUserAuthenticated = (req, res, next) => {
+const isUserAuthenticated = (req, res, next) => {
   req.user ? next() : res.sendStatus(401);
+};
+
+const isAuthenticated = (req, res, next) => {
+  const apiKey = req.get("API-Key");
+  if (apiKey && apiKey === process.env.API_KEY) {
+    next();
+  } else {
+    isUserAuthenticated(req, res, next);
+  }
+};
+
+module.exports = {
+  isUserAuthenticated,
+  isAuthenticated,
 };
