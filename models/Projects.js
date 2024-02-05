493317a53ed5c5c2124f0c87321cdb524f2d610d
Benoit Quette <benoit.quette@gmail.com>
2024-02-05 17:24:54 +0100

    added user Id in Project model

--- models/Projects.js
+++ models/Projects.js
@@ -13,15 +13,7 @@ var ProjectSchema = new mongoose.Schema(
     url: String,
     title: String,
     description: String,
-    crawling: {
-      type: String,
-      enum: ["no_crawling", "1", "2", "3", "all"],
-    },
-    verified: Boolean,
-    verificationToken: String,
-    iconUrl: String,
-    created: Date,
-    modified: Date,
+    user: mongoose.Types.ObjectId,
   },
   { timestamps: true }
 );
