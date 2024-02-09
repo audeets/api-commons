876cbb65365a45b3dedfdbbbb6b193cac0fce4e1
Benoit Quette <benoit.quette@gmail.com>
2024-02-09 10:25:29 +0100

    added common server

---
+++ server.js
@@ -0,0 +1,71 @@
+import https from "https";
+import http from "http";
+import fs from "fs";
+
+function createServer(app, defaultPort, defaultportSsl) {
+  // Create HTTPS server
+  if (process.env.NODE_ENV !== "development") {
+    var sslPort = normalizePort(process.env.PORT_HTTPS || defaultportSsl);
+    https
+      .createServer(
+        {
+          key: fs.readFileSync(`${process.env.PATH_SSL_KEY}`),
+          cert: fs.readFileSync(`${process.env.PATH_SSL_CERT}`),
+        },
+        app
+      )
+      .listen(sslPort)
+      .on("error", onError);
+  }
+
+  // Create HTTP server.
+  var port = normalizePort(process.env.PORT_HTTP || defaultPort);
+  http.createServer(app).listen(port).on("error", onError);
+}
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind =
+    typeof sslPort === "string" ? "Pipe " + sslPort : "Port " + sslPort;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+export { createServer };
