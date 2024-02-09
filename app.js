974157e8fc4a4de34d0e0e843cade06648aefe83
Benoit Quette <benoit.quette@gmail.com>
2024-02-09 10:40:48 +0100

    create common express app

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passport from "passport";
+import bodyParser from "body-parser";
+import { init } from "./auth/passport.js";
+
+const createApp = (allowMethods) => {
+  var app = express();
+  init();
+  app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_BASE_URL);
+    res.setHeader("Access-Control-Allow-Methods", allowMethods);
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 30,
+        httpOnly: false,
+      },
+      store: MongoStore.create({
+        mongoUrl: process.env.URL_MONGO,
+      }),
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+};
+
+export default { createApp };
