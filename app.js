05ebc2e724f2283affedb9f7802c1ad8f20cd803
Benoit Quette <benoit.quette@gmail.com>
2024-03-09 14:51:51 +0100

    try and fix session

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
-import passport from 'passport';
-import bodyParser from 'body-parser';
-import { init } from './auth/passport.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passport from "passport";
+import bodyParser from "body-parser";
+import { init } from "./auth/passport.js";
 
 const createApp = (allowMethods) => {
   var app = express();
   init();
   app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_BASE_URL);
-    res.setHeader('Access-Control-Allow-Methods', allowMethods);
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_BASE_URL);
+    res.setHeader("Access-Control-Allow-Methods", allowMethods);
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
     next();
   });
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
-      resave: false,
-      // rolling: true,
+      resave: true,
+      rolling: true,
       saveUninitialized: true,
       cookie: {
-        maxAge: 1000 * 60 * 30,
+        maxAge: 1000 * 60 * 30, // 30 mins
         httpOnly: false,
-        sameSite: 'none'
+        // sameSite: "none",
       },
       store: MongoStore.create({
-        mongoUrl: process.env.URL_MONGO
-      })
+        mongoUrl: process.env.URL_MONGO,
+      }),
     })
   );
   app.use(passport.initialize());
