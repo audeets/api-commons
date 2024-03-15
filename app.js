97f517d82bda26774060c9add56d56fcf9fa0adf
Benoit Quette <benoit.quette@gmail.com>
2024-03-15 13:42:08 +0100

    removed domain cookie conf

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import MongoStore from "connect-mongo";
-import passport from "passport";
-import bodyParser from "body-parser";
-import { init } from "./auth/passport.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import passport from 'passport';
+import bodyParser from 'body-parser';
+import { init } from './auth/passport.js';
 
 const createApp = (allowMethods) => {
   var app = express();
   init();
   app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_BASE_URL);
-    res.setHeader("Access-Control-Allow-Methods", allowMethods);
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_BASE_URL);
+    res.setHeader('Access-Control-Allow-Methods', allowMethods);
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
-  const domain = process.env.COOKIE_DOMAIN;
-  const domainConf = domain && domain !== "localhost" ? { domain } : {};
+  // const domain = process.env.COOKIE_DOMAIN;
+  const domainConf = {}; //domain && domain !== 'localhost' ? { domain } : {};
   app.use(
     session({
       name: process.env.COOKIE_NAME,
@@ -31,11 +28,11 @@ const createApp = (allowMethods) => {
       cookie: {
         ...domainConf,
         maxAge: 1000 * 60 * 30, // 30 mins
-        httpOnly: false,
+        httpOnly: false
       },
       store: MongoStore.create({
-        mongoUrl: process.env.URL_MONGO,
-      }),
+        mongoUrl: process.env.URL_MONGO
+      })
     })
   );
   app.use(passport.initialize());
